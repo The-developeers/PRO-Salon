5d740fadecf076d9f9bacb9bf132046051b74a19
mrpolar777 <costa.henrique9577@gmail.com>
2025-11-26 22:51:45 -0300

    Agendamento 1.4 [ajuste]

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import userRoutes from './user.routes.js'
 import employeeRoutes from './employee.routes.js'
 import serviceRoutes from './service.routes.js'
-// 1. Importar o arquivo REAL de rotas
 import appointmentRoutes from './appointment.routes.js' 
 import authRoutes from './auth.routes.js'
 
@@ -14,9 +13,6 @@ router.use('/auth', authRoutes)
 router.use('/users', requireAuth, requireRole('admin'), userRoutes)
 router.use('/employees', requireAuth, employeeRoutes)
 router.use('/services', requireAuth, serviceRoutes)
-
-// 2. Usar a rota real. 
-// O frontend chama '/agendamentos', mas usa o controller do backend.
 router.use('/agendamentos', requireAuth, appointmentRoutes)
 
 export default router
